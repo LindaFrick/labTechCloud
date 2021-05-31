4feae6baf826dcd9617c281ed4fd389ac31db5cd
LindaFrick <60000525+LindaFrick@users.noreply.github.com>
2021-05-31 13:23:16 +0200

    Update labda_suggest_handler.js

--- labda_suggest_handler.js
+++ labda_suggest_handler.js
@@ -27,20 +27,25 @@ module.exports.suggest = (event, context, callback) => {
         })
     }
     
-    async function search_by_tags(tags_combination){
-        console.log("ciao search")
-        //return talk.find(element => {tags_combination.every(v => element.tags.includes(v))})
-        return talk.find({ tags: { $in: ["TED","talks"] }})
-        .then(results => {
-            console.log(results)
+    function search_by_tags(tags_combination, documents){
+        var results = []
+        documents.forEach(element => 
+        {if(tags_combination.every(v => element.tags.includes(v))){results.push(element)}})
+        
             results.sort(function(talk1,talk2){
-                var comments1 = Number(talk1.comments_num.replace(".",""))
-                var comments2 = Number(talk2.comments_num.replace(".",""))
+                var comments1 = 0
+                var comments2 = 0
+                if(talk1.comments_num){
+                    comments1 = Number(talk1.comments_num.replace(".",""))
+                }
+                if(talk2.comments_num){
+                    comments2 = Number(talk2.comments_num.replace(".",""))
+                }
                 return comments1 - comments2;
             })
             
             return results[results.length-1]
-        });
+        
     }
     
         function combine(a, min) {
@@ -66,11 +71,50 @@ module.exports.suggest = (event, context, callback) => {
     }
     
     connect_to_db().then(() => {
-        var res = combine(body.tags,1)
-        console.log(res)
-        var talks_max_comments = []
-        res.forEach(combination => {talks_max_comments.push(search_by_tags(combination))})
-        Promise.all(talks_max_comments).then(value => console.log(value))
+        var combinations = combine(body.tags,1)
+        var map = {}
+        var comb_points = []
         
-    });
+        for(var i=0;i<body.tags.length;i++){
+            map[body.tags[i]] = body.nums[i];
+        }
+        
+        console.log(combinations)
+        var talks_max_comments = []
+        talk.find().then(documents => {
+            
+            combinations.forEach(combination => {
+                talks_max_comments.push(search_by_tags(combination, documents));
+                let points = 0
+                combination.forEach(tag => {points += map[tag]})
+                comb_points.push(points)
+                
+            })
+            var max = 0
+            var winnerID
+            for(var i = 0; i<combinations.length;i++){
+                if(talks_max_comments[i]){
+                    if(!talks_max_comments[i].comments_num){
+                        talks_max_comments[i].comments_num = 0
+                    }
+                    let score = talks_max_comments[i].comments_num * comb_points[i]
+                    if(score>max){
+                        max = score
+                        winnerID = talks_max_comments[i].id
+                    }
+                }
+            }
+             callback(null, {
+                    statusCode: 200,
+                        body: "suggeriamo: " + winnerID
+                })
+        });
+    })
+     .catch(err =>
+                callback(null, {
+                    statusCode: err.statusCode || 500,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the talks.'
+                })
+            );
 };
